e452e61135a056fa33124303facd45ad11981c43
harujiiiii <>
2021-05-30 13:32:29 +0000

    I saved the diagram as a PNG file named temp_line_plot.png.

--- Exercise_7_problem_1.py
+++ Exercise_7_problem_1.py
@@ -63,12 +63,16 @@ plt.scatter(x,y,s=50,c=colors,cmap='rainbow',edgecolor='black')
 
 # Add labels and title
 # YOUR CODE HERE 4
+plt.title("My random candy points")
+plt.xlabel("x-label")
+plt.ylabel("Y-label")
 
 # Save the plot as a png file:
 outputfp = "my_first_plot.png"
 
 # YOUR CODE HERE 5
-
+plt.savefig(outputfp)
+plt.show()
 # This test print statement should print the output filename of your figure
 print("Saved my first plot as:", outputfp)
 
